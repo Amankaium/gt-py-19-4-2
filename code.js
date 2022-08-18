b40ed6e9d648b9e833633275fce7d25558a5743f
Amankaium <kaium@list.ru>
2022-08-18 15:30:44 +0600

    first commit

---
+++ code.js
@@ -0,0 +1,34 @@
+// console.log("hello world")
+//
+// var a = 3
+// let b = 2
+// const c = 4
+//
+// a = 6
+// b = 8
+// console.log(a, b, c)
+//
+// if (a > 5) {
+//     console.log('a больше')
+// }
+//
+// for (let i=0; i<3; i++) {
+//     console.log(i)
+// }
+//
+// function someRes(k, m) {
+//     return k + m
+// }
+//
+// console.log(someRes(7, 10))
+
+
+const myButton = document.querySelector("button")
+myButton.addEventListener("click", out)
+
+function out() {
+    // const dt = new Date()
+    // console.log(dt)
+    const h2 = document.querySelector("#test")
+    h2.style.color = "green"
+}
